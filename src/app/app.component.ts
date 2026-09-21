ad4a52ad0f249f19a7e4bb72c0c0c12eae3a311f
Axel Jäger <axeljaeger@gmail.com>
2026-09-21 15:46:03 +0200

    Render on demand using newer angular mechanism (#276)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { View3dComponent } from './view3d/smart-components/view3d/view3d.component';
+import { LiteRendererViewComponent } from './view3d/smart-components/lite-renderer-view/lite-renderer-view.component';
 import { ResultContainerComponent } from './sidebar/pure-components/result-container/result-container.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { SidebarContainerComponent } from './sidebar/pure-components/sidebar-container/sidebar-container.component';
@@ -9,6 +9,11 @@ import { SidebarContainerComponent } from './sidebar/pure-components/sidebar-con
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [SidebarContainerComponent, View3dComponent, ResultContainerComponent, MatToolbarModule],
+  imports: [
+    SidebarContainerComponent,
+    LiteRendererViewComponent,
+    ResultContainerComponent,
+    MatToolbarModule,
+  ],
 })
 export class AppComponent {}
